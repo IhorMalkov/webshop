d75bf3f4914487804b03251a0f6c5648fdde8933
IhorMalkov <igorzno2004@gmail.com>
2024-07-30 11:42:27 -0700

    latest changes

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -4,8 +4,8 @@ import Slider from "../components/Slider";
 
 function Shop() {
   return (
-      <div>
-          <div className="flex justify-between items-center ml-[30px] mr-[30px]">
+      <div className="pl-[30px] pr-[30px]">
+          <div className="flex justify-between items-center">
               <h1>iPad</h1>
               <h2>Touch, draw and type <br/> on one magical device.</h2>
           </div>
