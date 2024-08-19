c44e93762f549f61cbbde16f4b9d8b34481f632e
IhorMalkov <igorzno2004@gmail.com>
2024-08-19 14:31:25 -0700

    Made code cleaner

---
+++ src/products.js
@@ -0,0 +1,47 @@
+import ipadPro from './img/ipadPro.png';
+import ipadAir from './img/ipadAir.png';
+import ipad from './img/iPad.png';
+import ipadMini from './img/ipadMini.png';
+
+export const product = [
+  {
+    img: ipadPro,
+    title: 'iPad Pro',
+    info: 'The ultimate iPad experience with the most advanced technology.',
+    prices: 'From $1399 or $119.76/mo. for 12 mos. at 4.99 % APR',
+    id: 1,
+    price: 1399,
+    totalPrice: 0,
+    quantity: 0
+  },
+  {
+    img: ipadAir,
+    title: 'iPad Air',
+    info: 'Serious performance in a thin and light design.',
+    prices: 'From $799 or $68.40/mo. for 12 mos. at 4.99 % APR‡',
+    id: 2,
+    price: 799,
+    totalPrice: 0,
+    quantity: 0
+  },
+  {
+    img: ipad,
+    title: 'iPad',
+    info: 'The colourful, all‑screen iPad for the things you do every day.',
+    prices: 'From $499 or $42.72/mo. for 12 mos. at 4.99 % APR‡',
+    id: 3,
+    price: 499,
+    totalPrice: 0,
+    quantity: 0
+  },
+  {
+    img: ipadMini,
+    title: 'iPad Mini',
+    info: 'The full iPad experience in an ultra portable design.',
+    prices: 'From $679 or $58.12/mo. for 12 mos. at 4.99 % APR‡',
+    id: 4,
+    price: 679,
+    totalPrice: 0,
+    quantity: 0
+  }
+];
